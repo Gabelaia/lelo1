0631f81a45a596bde7c4902d7037cfffb1800d4d
Gabelaia <lela.gabelaia@gmail.com>
2024-11-08 18:28:19 +0400

    Third Project about Strings

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,12 @@
+package org.example;
+
+public class Main {
+
+
+
+    public static void main(String[] args) {
+        String fullName = "Lela AGabelaia";
+        String fullName1 = "Lela Gabelaia";
+        System.out.println(fullName.compareTo(fullName1));
+    }
+}
